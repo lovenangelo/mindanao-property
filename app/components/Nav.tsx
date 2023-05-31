73d0682335e77c6e250b5cdd030e597af8e46de9
lovenangelo <lovenangelo.dev@gmail.com>
2023-05-31 16:21:42 +0800

    changed nav color to white

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -16,7 +16,7 @@ const Nav = () => {
   };
 
   return (
-    <div className="navbar bg-base-200 bg-opacity-15">
+    <div className="navbar bg-white">
       <div className="flex-1 ml-28 ">
         <Link href="/" className="btn btn-ghost normal-case text-xl">
           <img className="h-12 w-44" src="/assets/logo/logo.png" alt="" />
@@ -25,31 +25,42 @@ const Nav = () => {
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1 mr-16">
           <li>
-            <Link className="font-sans font-medium" href={"/"}>Home</Link>
+            <Link className="font-sans font-medium" href={"/"}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link className="font-sans font-medium" href={"/"}>Contact</Link>
+            <Link className="font-sans font-medium" href={"/"}>
+              Contact
+            </Link>
           </li>
           <li>
-            <Link className="font-sans font-medium" href={"/"}>Pricing</Link>
+            <Link className="font-sans font-medium" href={"/"}>
+              Pricing
+            </Link>
           </li>
           <li>
-            <Link className="font-sans font-medium" href={"/properties"}>Properties</Link>
+            <Link className="font-sans font-medium" href={"/properties"}>
+              Properties
+            </Link>
           </li>
           {user ? (
             <li>
-                   <button className="font-sans font-medium" onClick={handleSignOut}>Sign out</button>
+              <button className="font-sans font-medium" onClick={handleSignOut}>
+                Sign out
+              </button>
             </li>
           ) : (
             <li>
-              <Link className="font-sans font-medium" href={"/auth"}>Sign in</Link>
+              <Link className="font-sans font-medium" href={"/auth"}>
+                Sign in
+              </Link>
             </li>
           )}
         </ul>
       </div>
     </div>
     // Hero Section
-    
   );
 };
 
