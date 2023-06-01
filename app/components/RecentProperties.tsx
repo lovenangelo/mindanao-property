f3d30ad121be8ade577a769299c7d343333eba52
bug-dbg <90324449+bug-dbg@users.noreply.github.com>
2023-06-01 16:24:36 +0800

    Updated some styling in the recentProp component

--- app/components/RecentProperties.tsx
+++ app/components/RecentProperties.tsx
@@ -1,42 +1,113 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+
 const RecentProperties = () => {
   return (
+   
     <section>
-      <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8 min-h-screen">
-        <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
-          <div className="relative z-10 lg:py-16">
-            <div className="relative h-64 sm:h-80 lg:h-full">
-              <img
-                alt="House"
-                src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="absolute inset-0 h-full w-full object-cover"
-              />
-            </div>
-          </div>
-
-          <div className="relative flex items-center bg-gray-100">
-            <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"></span>
-
-            <div className="p-8 sm:p-16 lg:p-24">
-              <h2 className="text-2xl font-bold sm:text-3xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Tempore, debitis.
-              </h2>
-
-              <p className="mt-4 text-gray-600">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aliquid, molestiae! Quidem est esse numquam odio deleniti,
-                beatae, magni dolores provident quaerat totam eos, aperiam
-                architecto eius quis quibusdam fugiat dicta.
-              </p>
-
-              <a
-                href="#"
-                className="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+      <div className="flex flex-col text-center">
+        <h1 className="text-5xl font-serif font-bold text-secondary">
+          Similar Properties
+        </h1>
+        <p className="font-sans  py-6  ml-32 mr-32 mb-10 items-center">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+        </p>
+      </div>
+      <div className=" flex content-center w-3/4 ml-40 flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <img
+            src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="p-6">
+         
+          <h4 className="font-serif text-secondary mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Lyft launching cross-platform service this week
+          </h4>
+          <p className="mb-12 font-sans textmb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+            Like so many organizations these days, Autodesk is a company in
+            transition. It was until recently a traditional boxed software company
+            selling licenses. Yet its own business model disruption is only part of
+            the story
+          </p>
+          <a className="inline-block" href="#">
+            <button
+              className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase btn btn-outline btn-primary"
+              type="button"
+            >
+              More Info
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+                className="h-4 w-4"
               >
-                Get in Touch
-              </a>
-            </div>
-          </div>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </a>
+        </div>
+      </div>
+
+      {/* other similar properties */}
+      <div className="mt-12 mb-12 relative flex w-3/4 ml-40 flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg ">
+      
+        <div className="p-6">
+          
+          <h4 className="font-serif text-secondary mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Lyft launching cross-platform service this week
+          </h4>
+          <p className="font-sans mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+            Like so many organizations these days, Autodesk is a company in
+            transition. It was until recently a traditional boxed software company
+            selling licenses. Yet its own business model disruption is only part of
+            the story
+          </p>
+          <a className="inline-block" href="#">
+            <button
+              className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase btn btn-outline btn-primary"
+              type="button"
+            >
+              More Info
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                aria-hidden="true"
+                className="h-4 w-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </a>
+        </div>
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-l-none bg-white bg-clip-border text-gray-700">
+          <img
+            src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
+            alt="image"
+            className="h-full w-full object-cover"
+          />
         </div>
       </div>
     </section>
