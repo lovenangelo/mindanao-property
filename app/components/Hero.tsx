3b79b75fd6e881df949d783538251d826933afae
bug-dbg <90324449+bug-dbg@users.noreply.github.com>
2023-06-02 12:55:53 +0800

    Updated hero component styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,28 @@
 import Search from "./Search";
 
+
 const Hero = () => {
+
+  const style:React.CSSProperties = {
+    position: "absolute", 
+    height: "inherit",
+    width: "100%",
+    top: "10",
+    left: "0",
+    background: "rgba(0,0,0,0.30)",
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  }
+  
   return (
     <div
-      className="hero container-lg min-h-screen" 
-      style={{ backgroundImage: "url(/assets/img/Hero.png)" }}
+      className="hero container-lg" 
+      style={{ backgroundImage: "url(/assets/img/Hero.png)", height: "80vh", display: "flex" }}
     >
-      <div className="hero-overlay bg-opacity-60" />
+      <div className="overlay bg-opacity-60" style={style} />
       <div className="hero-content" >
         <div>
           <div className="flex items-center mt-48">
