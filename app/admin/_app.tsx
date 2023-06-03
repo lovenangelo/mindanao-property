8d8d63c0831aaa5fc8db8e2105d77b42965ab059
bug-dbg <90324449+bug-dbg@users.noreply.github.com>
2023-06-03 22:04:56 +0800

    admin page

---
+++ app/admin/_app.tsx
@@ -0,0 +1,42 @@
+// "use client";
+
+// import '../styles/globals.css';
+// import type {AppProps} from 'next/app';
+// import {createTheme, NextUIProvider} from '@nextui-org/react';
+// import {ThemeProvider as NextThemesProvider} from 'next-themes';
+// import {Layout} from './layout';
+
+// const lightTheme = createTheme({
+//    type: 'light',
+//    theme: {
+//       colors: {},
+//    },
+// });
+
+// const darkTheme = createTheme({
+//    type: 'dark',
+//    theme: {
+//       colors: {},
+//    },
+// });
+
+// function MyApp({Component, pageProps}: AppProps) {
+//    return (
+//       <NextThemesProvider
+//          defaultTheme="system"
+//          attribute="class"
+//          value={{
+//             light: lightTheme.className,
+//             dark: darkTheme.className,
+//          }}
+//       >
+//          <NextUIProvider>
+//             <Layout>
+//                <Component {...pageProps} />
+//             </Layout>
+//          </NextUIProvider>
+//       </NextThemesProvider>
+//    );
+// }
+
+// export default MyApp;
---
+++ app/admin/account.tsx
@@ -0,0 +1,9 @@
+// "use client";
+// import React from 'react';
+// import {Accounts} from './components/accounts';
+
+// const accounts = () => {
+//    return <Accounts />;
+// };
+
+// export default accounts;
