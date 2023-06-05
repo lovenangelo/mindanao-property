cd82e7fd257ef378379198faf471ba3e6649072d
lovenangelo <lovenangelo.dev@gmail.com>
2023-06-05 12:10:10 +0800

    added loading

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@
 import Guest from "./components/Guest";
 import { useUser } from "./providers/UserProvider";
 import Authenticated from "./components/Authenticated";
+import Loading from "./loading";
 
 export default function Home() {
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <main className="">{user == null ? <Guest /> : <Authenticated />}</main>
