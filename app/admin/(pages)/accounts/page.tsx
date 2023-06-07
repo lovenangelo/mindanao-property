77b7aea0e979b2389a161fe38d89656269e7f5ee
lovenangelo <lovenangelo.dev@gmail.com>
2023-06-08 07:17:44 +0800

    clean

--- app/admin/(pages)/accounts/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from 'react';
-import {Accounts} from '../../components/accounts/index';
-
-const accounts = () => {
-   return <Accounts />;
-};
-
-export default accounts;
