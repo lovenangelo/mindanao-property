07f2fcd6ef01b674d7a456f56705fcb160d567fb
lovenangelo <lovenangelo.dev@gmail.com>
2023-06-09 14:20:49 +0800

    changed loaders, moved toast prov to folder

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import OverviewLoader from "@/components/loaders/overview-loader-skeleton"
+import MainLoader from "@/components/loaders/main-loader"
 import { useUser } from "@/components/providers/user-provider"
 
 export default function AccountPage() {
   const { isLoading, user } = useUser()
 
   if (isLoading || !user) {
-    return <OverviewLoader />
+    return <MainLoader />
   }
 
   return <div>Account page</div>
--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
-import OverviewLoader from "@/components/loaders/overview-loader-skeleton"
+import MainLoader from "@/components/loaders/main-loader"
 import { useUser } from "@/components/providers/user-provider"
 
 export default function DashboardPage() {
   const { isLoading, user } = useUser()
+  console.log(isLoading, user)
 
   if (isLoading || !user) {
-    return <OverviewLoader />
+    return <MainLoader />
   }
 
   return <div className="p-4">OverviewPage</div>
