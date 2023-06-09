8d5c3d19904490beee4b69a8af848fa55158f166
lovenangelo <lovenangelo.dev@gmail.com>
2023-06-09 12:26:18 +0800

    change redirect route after sign up to auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,5 +14,5 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL("/dashboard", request.url))
+  return NextResponse.redirect(new URL("/auth", request.url))
 }
