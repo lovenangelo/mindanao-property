7a0dddb70b6199a39b4f500203e287066ad45397
lovenangelo <lovenangelo.dev@gmail.com>
2023-06-19 15:10:05 +0800

    integration of google maps

---
+++ app/(authenticated-routes)/properties/components/google-map.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import React from "react"
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
+
+const GMap = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  })
+
+  const [map, setMap] = React.useState<
+    null | boolean | Error | google.maps.Map
+  >(null)
+
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center)
+    map.fitBounds(bounds)
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  )
+}
+
+export default React.memo(GMap)
--- app/(authenticated-routes)/properties/page.tsx
+++ app/(authenticated-routes)/properties/page.tsx
@@ -1,9 +1,14 @@
 import { Input } from "@/components/ui/input"
 
+import GMap from "./components/google-map"
+
 export default function PropertiesPage() {
   return (
     <div>
       <Input placeholder="Search" />
+      <div>
+        <GMap />
+      </div>
     </div>
   )
 }
