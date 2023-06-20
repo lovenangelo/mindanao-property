4143e0e279d2e8033b18caee7faf788f9dc4df55
bug-dbg <90324449+bug-dbg@users.noreply.github.com>
2023-06-20 20:48:43 +0800

    chore: change overview and notification UI styling

---
+++ app/(authenticated-routes)/overview/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import MainLoader from "@/components/loaders/main-loader"
+import { useUser } from "@/components/providers/user-provider"
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+
+export default function AccountLayout({ children }: RootLayoutProps) {
+  const { isLoading, user } = useUser()
+
+  if (isLoading || !user) {
+    return <MainLoader />
+  }
+
+  return (
+    <div className="h-[calc(100vh-65px)] w-full flex">
+      <div className="m-2 h-[calc(100%-16px)] md:my-4 md:mr-8 bg-background md:h-[calc(100%-32px)] w-full rounded-md">
+        {children}
+      </div>
+    </div>
+  )
+}
